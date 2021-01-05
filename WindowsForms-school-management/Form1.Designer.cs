f0c6c2cccca716d218d44e5885d9056c49ff476a
Lundwe Mukuka <Lundwe.Mukuka@codedean.com>
2021-01-05 15:02:41 +0000

    commit all

---
+++ WindowsForms-school-management/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsForms_school_management
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.label1 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.button1 = new System.Windows.Forms.Button();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Location = new System.Drawing.Point(250, 254);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(88, 18);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "User Name";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(253, 273);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(303, 23);
+      this.textBox1.TabIndex = 1;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(253, 334);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(303, 23);
+      this.textBox2.TabIndex = 3;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label2.Location = new System.Drawing.Point(250, 315);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(75, 18);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "Password";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label3.Location = new System.Drawing.Point(431, 374);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(125, 18);
+      this.label3.TabIndex = 4;
+      this.label3.Text = "Forget Password";
+      // 
+      // button1
+      // 
+      this.button1.ForeColor = System.Drawing.Color.ForestGreen;
+      this.button1.Location = new System.Drawing.Point(253, 404);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(303, 23);
+      this.button1.TabIndex = 5;
+      this.button1.Text = "Sign In";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+      this.pictureBox1.Location = new System.Drawing.Point(305, 39);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(197, 177);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+      this.pictureBox1.TabIndex = 6;
+      this.pictureBox1.TabStop = false;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(771, 554);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label1);
+      this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+  }
+}
+
